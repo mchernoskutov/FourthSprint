47a5d542f29cc195b3df41a824c3682dec13ccf8
Chernoskutov Mikhail <mchernoskytovyan@yandex.ru>
2024-06-19 18:18:13 +0100

    Parametrization for FaqTest

--- src/test/java/faqtest/FaqTest.java
+++ src/test/java/faqtest/FaqTest.java
@@ -1,64 +1,76 @@
 package faqtest;
 
-import org.junit.After;
-import org.junit.Assert;
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.openqa.selenium.WebDriver;
-import org.junit.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import ya.practikum.pages.MainPage;
 
 import static ya.practikum.constants.TestData.FAQ_ANSWER;
-import static ya.practikum.constants.TestData.FAQ_HEAD;
-
-public class FaqTest {
-
-    private WebDriver driver;
 
 
 
+@RunWith(Parameterized.class)
+public class FaqTestP {
 
-    //проверяем выпадающий список FAQ
+     private WebDriver driver;
+     private MainPage objMainPage;
+
+     //для параметризации
+     private final int faqNumber;
+
+     public FaqTestP (int faqNumber) {
+         this.faqNumber = faqNumber;
+
+     }
+     //открываем браузер и скроллим до вопросов перед тестами
+     @Before
+     public void open() {
+
+         driver = new ChromeDriver();
+         objMainPage = new MainPage(driver);
+
+         //открываем страницу
+         objMainPage.openPage();
+
+         //скролл до FAQ
+         objMainPage.scrollToFAQ();
+     }
+
+    //номер вопроса и ответа для теста
+    @Parameterized.Parameters
+    public static Object[] getNumber() {
+        return new Object[][] {
+                {0},
+                {1},
+                {2},
+                {3},
+                {4},
+                {5},
+                {6},
+                {7},
+
+        };
+    }
+
+
+    //каждый тест на одну пару вопроса и ответа
     @Test
-    public void checkFaq() {
+    public void faqTest (){
 
-        driver = new ChromeDriver();
-        //driver = new FirefoxDriver();
+        //проверяем, что виден вопрос и не виден ответ в начале теста
+        Assert.assertTrue("Вопрос FAQ: "+faqNumber+ " не виден в начале теста", objMainPage.isTextAccordeonHeadVisible(faqNumber));
+        Assert.assertFalse("Ответ FAQ: "+faqNumber+" не скрыт в начале теста",objMainPage.isTextAccordeonAnswerHidden(faqNumber));
 
-        //создаем главную страницу
-        MainPage objMainPage = new MainPage(driver);
+        //кликаем на вопрос
+        objMainPage.clickAccordeonHead(faqNumber);
 
-        //открываем страницу
-        objMainPage.openPage();
+        //проверяем видимость ответа
+        Assert.assertTrue("Ответ FAQ: " + faqNumber + " не видно после клика",objMainPage.isTextAccordeonAnswerVisible(faqNumber));
+        //проверяем текст ответа
+        Assert.assertEquals("Текст ответа FAQ: " + faqNumber + " не соответствует ожидаемому", FAQ_ANSWER[faqNumber],objMainPage.getTextAccordeonAnswer(faqNumber));
 
-        //скролл до FAQ
-        objMainPage.scrollToFAQ();
-
-        //проверяем, что все ответы изначально скрыты, все вопросы видны
-        for (int i = 0; i < FAQ_HEAD.length; i++) {
-            Assert.assertTrue("Вопрос FAQ: "+i+ " не виден при открытии страницы", objMainPage.isTextAccordeonHeadVisible(i));
-            Assert.assertFalse("Ответ FAQ: "+i+" не скрыт при открытии страницы",objMainPage.isTextAccordeonAnswerHidden(i));
-        }
-
-        //нажимаем последовательно на пункты, проверяем текст, проверяем, что остальные ответы закрываются
-        for (int i = 0; i < FAQ_HEAD.length; i++) {
-
-            //кликаем на вопрос
-            objMainPage.clickAccordeonHead(i);
-
-            //проверяем, что видно ответ, другие ответы скрыты
-            for (int j = 0; j < FAQ_HEAD.length; j++) {
-                if (i != j ) {     //при несовпадениее номера вопроса и ответа
-                    //проверяем, что другие ответы скрыты
-                    Assert.assertFalse("Ответ FAQ: " + j + " не скрыт при клике на вопрос: " + i , objMainPage.isTextAccordeonAnswerHidden(j));
-                } else { //при совпадении номера вопроса и ответа
-                    //проверяем, что ответ видно
-                    Assert.assertTrue("Ответ FAQ: " + i + " не видно после клика",objMainPage.isTextAccordeonAnswerVisible(i));
-                    //проверяем текст ответа
-                    Assert.assertEquals("Текст ответа FAQ: " + i + " не соответствует ожидаемому", FAQ_ANSWER[i],objMainPage.getTextAccordeonAnswer(i));
-                }
-            }
-        }
     }
 
     @After
@@ -66,5 +78,6 @@ public class FaqTest {
         // Закрыть браузер после теста
         driver.quit();
     }
+
 
 }
